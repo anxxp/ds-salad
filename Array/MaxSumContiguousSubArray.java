6cad597612480797fcd277f0f716eb3dfb4a7388
Anoop Kumar <anoop.kumar@iongroup.com>
2018-05-30 18:13:39 +0530

    Max sum for a contiguous sub-array : Modified to keep track of the start and end index of the sub-array

--- Array/MaxSumContiguousSubArray.java
+++ Array/MaxSumContiguousSubArray.java
@@ -8,23 +8,28 @@ import java.sql.SQLOutput;
 
 public class MaxSumContiguousSubArray {
   public static void main(String args[]) {
-    int[] array = {1, -1, -3, 4};
+    int[] array = {1, -9, -3, 4};
 
-      System.out.println("Maximum sum of a contigous subarray is : "+maxSumSubArray(array));
+      System.out.println(" has maximum sum for a contigous subarray : "+maxSumSubArray(array));
   }
 
   public static int maxSumSubArray(int[] array) {
     int maxSumEndingHere =0;
     int maxSumSoFar =0;
+    int start_index =0;
+    int end_index =0;
     for (int i = 0; i < array.length; i++) {
         maxSumEndingHere = maxSumEndingHere+array[i];
+        end_index=i;
         if(maxSumEndingHere<0){
             maxSumEndingHere=0;
+            start_index=i+1;
         }
         if(maxSumEndingHere>maxSumSoFar){
             maxSumSoFar=maxSumEndingHere;
         }
     }
+    System.out.print("The sub-array starting from "+start_index+" and ending at "+end_index);
     return maxSumSoFar;
   }
 }
