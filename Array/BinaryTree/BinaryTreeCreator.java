95162efcac291e6dfffb61f1668e72683afbb936
anoop29 <anoopkumar.inbox@gmail.com>
2018-06-11 23:25:51 +0530

    Binary Tree creator . This would be used to check our code against various tree structures.

---
+++ Array/BinaryTree/BinaryTreeCreator.java
@@ -0,0 +1,37 @@
+package Array.BinaryTree;
+
+public class BinaryTreeCreator {
+
+
+    public static Node createInputBinaryTree(int type){
+        Node root;
+
+        switch(type){
+            case 0:
+                root = new Node(40);
+                break;
+            case 1:
+                root = new Node(40);
+                root.left = new Node(25);
+                root.left.right = new Node(30);
+                root.left.left = new Node(15);
+                root.right = new Node(50);
+                root.right.left = new Node(45);
+                root.right.right = new Node(55);
+                root.right.right.right = new Node(75);
+                root.right.right.right.right = new Node(100);
+                break;
+                case 2:
+                root = new Node(40);
+                root.right = new Node(50);
+                root.right.right = new Node(55);
+                root.right.right.right = new Node(75);
+                break;
+            default:
+                root = null;
+        }
+
+        return root;
+
+    }
+}
