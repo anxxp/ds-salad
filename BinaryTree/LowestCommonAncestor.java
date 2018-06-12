822ec8185be33839f658b079756995392bdd96d9
anoop29 <anoopkumar.inbox@gmail.com>
2018-06-12 23:37:01 +0530

    Lowest Common ancestor. This code works with the assumption that both the numbers are present in the Tree

--- BinaryTree/LowestCommonAncestor.java
+++ BinaryTree/LowestCommonAncestor.java
@@ -2,5 +2,32 @@ package BinaryTree;
 
 public class LowestCommonAncestor {
 
+    public static void main(String a[]){
+        Node root = BinaryTreeCreator.createInputBinaryTree(1);
+        System.out.println(lowestCommonAncestor(root,45,100).data);
 
+    }
+
+    public static Node lowestCommonAncestor(Node node,int firstNum,int secondNum){
+
+        if(node==null){
+            return null;
+        }
+
+        if(node.data==firstNum || node.data ==secondNum){
+            return node;
+        }
+
+        Node fromLeft = lowestCommonAncestor(node.left,firstNum,secondNum);
+        Node fromRight = lowestCommonAncestor(node.right,firstNum,secondNum);
+
+        if(fromLeft==null && fromRight== null){
+            return null;
+        }
+        if(fromLeft!=null && fromRight!=null){
+            return node;
+        }
+
+        return fromLeft!=null?fromLeft:fromRight;
+    }
 }
