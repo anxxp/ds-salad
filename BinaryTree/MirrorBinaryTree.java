c8ff5ec729655bb7a16843c00414b7670612c4e0
anoop29 <anoopkumar.inbox@gmail.com>
2018-06-19 00:37:56 +0530

    Check if the Binary Trees are mirror of each other

---
+++ BinaryTree/MirrorBinaryTree.java
@@ -0,0 +1,38 @@
+package BinaryTree;
+
+public class MirrorBinaryTree {
+
+    public static void main(String args[]){
+
+        Node rootFirst = BinaryTreeCreator.createInputBinaryTree(1);
+        Node rootSecond = BinaryTreeCreator.createInputBinaryTree(4);
+
+        System.out.println(checkIfMirrorBinaryTree(rootFirst,rootSecond));
+
+    }
+    /**
+     * If both the nodes are NULL or same, it means they are mirror.
+     * The code will look same as the one for Same Binary Tree.
+     * The difference between both the code is the argument which we pass to
+     * the function. In case of Mirror we pass the Left Child of one and Right Child
+     * of other.
+     * */
+    public static boolean checkIfMirrorBinaryTree(Node firstNode,Node secondNode ){
+
+        if(firstNode == null && secondNode == null){
+
+            return true;
+
+        }else if(firstNode == null || secondNode == null){
+
+            return false;
+
+        }else if(firstNode.data == secondNode.data){
+
+            return checkIfMirrorBinaryTree(firstNode.left,secondNode.right) &&
+                    checkIfMirrorBinaryTree(firstNode.right,secondNode.left);
+        }
+
+        return false;
+    }
+}
