e55bf6619244c9128806883d2b9e1f9853e0dd6f
Anoop Kumar <anoop.kumar@iongroup.com>
2018-06-18 15:56:15 +0530

    Check if given two Binary Trees are same

---
+++ BinaryTree/SameBInaryTree.java
@@ -0,0 +1,31 @@
+package BinaryTree;
+
+public class SameBInaryTree {
+
+    public static void main(String args[]){
+        Node rootNodeFirst = BinaryTreeCreator.createInputBinaryTree(1);
+        Node rootNodeSecond = BinaryTreeCreator.createInputBinaryTree(3);
+        System.out.println(checkIfSameBinaryTree(rootNodeFirst,rootNodeSecond));
+
+    }
+    /**
+     * The logic for checking if both the Trees are same is:
+     *  1. If both the nodes are NULL then, they both would be leaf nodes. Return TRUE
+     *  2. If one of the nodes is NULL then, one is leaf node and other is not. Return FALSE
+     *  3. If both the nodes are equal then,
+     *      3.1 Check left sub-tree of both the nodes is same
+     *      3.2 If the left sub-tree is same then, we check for right sub-tree
+     *  4. In all other cases we return FALSE;
+     * */
+    public static boolean checkIfSameBinaryTree(Node nodeFirst,Node nodeSecond){
+
+        if(nodeFirst==null && nodeSecond==null){
+            return true;
+        }else if(nodeFirst==null || nodeSecond==null){
+            return false;
+        }else if(nodeFirst.data==nodeSecond.data){
+            return checkIfSameBinaryTree(nodeFirst.left,nodeSecond.left) || checkIfSameBinaryTree(nodeFirst.right,nodeSecond.right);
+        }
+        return false;
+    }
+}
