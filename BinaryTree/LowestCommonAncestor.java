5a3fe19bd03da79baff874e9953d51f9fc2d2b3a
anoop29 <anoopkumar.inbox@gmail.com>
2018-06-21 23:19:26 +0530

    Added comments to explain the algorithm.

--- BinaryTree/LowestCommonAncestor.java
+++ BinaryTree/LowestCommonAncestor.java
@@ -1,5 +1,17 @@
 package BinaryTree;
 
+/**
+ * NOTE : This program works with the assumption that both the nodes are present in the Tree.
+ *
+ * We follow the following approach:
+ *  1. When ever we encounter a node with data same as on of the given node,
+ *      we return the current Node.
+ *  2. If we get a NOT NULL value from both left and right sub-tree,
+ *      we return the current Node.
+ *  3. If we get NOT NULL from one sub-tree and NULL from other sub-tree then,
+ *      we return the NOT NULL node received.
+ *  4. Else , in all other cases we return NULL.
+ * */
 public class LowestCommonAncestor {
 
     public static void main(String a[]){
