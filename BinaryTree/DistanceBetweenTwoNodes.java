ba3ee421cb9e43b858f2c119ad76c06e0d82f28e
anoop29 <anoopkumar.inbox@gmail.com>
2018-06-22 01:56:11 +0530

    Distance between two Nodes correct approach

--- BinaryTree/DistanceBetweenTwoNodes.java
+++ BinaryTree/DistanceBetweenTwoNodes.java
@@ -2,50 +2,21 @@ package BinaryTree;
 
 public class DistanceBetweenTwoNodes {
 
-    static boolean hasFirstNode = false;
-    static boolean hasSecondNode = false;
     public static void main(String args[]){
 
         Node root = BinaryTreeHelper.createInputBinaryTree(1);
-        int firstNum  =  15;
-        int secondNum = 45;
+        int firstNum  =  25;
+        int secondNum = 55;
 
         System.out.println(distanceBetweenNodes(root,firstNum,secondNum));
 
     }
     public static int distanceBetweenNodes(Node node,int firstNum, int secondNum) {
 
-        if (node == null) {
-            return 0;
-        }
-
-        int leftDistance = distanceBetweenNodes(node.left, firstNum, secondNum);
-        int rightDistance = distanceBetweenNodes(node.right, firstNum, secondNum);
-
-        if(node.data == firstNum){
-            hasFirstNode =true;
-            if(leftDistance+rightDistance>0){
-                return leftDistance+rightDistance;
-            }else{
-                return 1;
-            }
-        }
-        if(node.data == secondNum){
-            hasSecondNode = true;
-            if(leftDistance+rightDistance>0){
-                return leftDistance+rightDistance;
-            }else{
-                return 1;
-            }
-        }
-        if(hasFirstNode && hasSecondNode){
-            return leftDistance+rightDistance;
-        }else if(leftDistance+rightDistance > 0){
-            return leftDistance+rightDistance+1;
-        }
-
-        return leftDistance + rightDistance;
-
+        Node lca = LowestCommonAncestor.lowestCommonAncestor(node,firstNum,secondNum);
+        int heightFirstNum = RootToNodeDistance.rootToNodeDistance(lca,firstNum);
+        int heightSecondNum = RootToNodeDistance.rootToNodeDistance(lca,secondNum);
+        return heightFirstNum + heightSecondNum;
     }
 
 }
