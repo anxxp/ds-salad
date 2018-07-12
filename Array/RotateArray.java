5caebc2ffcba2203192dea12a99596235b6d6668
anoop29 <anoopkumar.inbox@gmail.com>
2018-07-13 00:23:04 +0530

    Rotate an array by given number of positions

---
+++ Array/RotateArray.java
@@ -0,0 +1,31 @@
+package Array;
+
+import static Array.ArrayUtility.swap;
+
+public class RotateArray {
+
+    public static void main(String args[]){
+        int input[] ={4,7,3,9,7,1,2};
+        int numberOfStifts = 3;
+        int numberOfElements = input.length;
+
+        ArrayUtility.printArray("INPUT",input);
+        rotateArray(input,numberOfStifts,numberOfElements);
+        ArrayUtility.printArray("OUTPUT",input);
+    }
+
+    private static void rotateArray(int[] input, int numberOfStifts, int numberOfElements) {
+        reverse(input,0,numberOfStifts-1);
+        reverse(input,numberOfStifts,numberOfElements-1);
+        reverse(input,0,numberOfElements-1);
+    }
+
+    private static void reverse(int[] input, int start, int end) {
+
+        while(start < end){
+            swap(input,start,end);
+            start++;
+            end--;
+        }
+    }
+}
