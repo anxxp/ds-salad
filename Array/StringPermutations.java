cf1436e63f58c0ecc3f0442c44a7d3ced62ea942
anoop29 <anoopkumar.inbox@gmail.com>
2018-07-16 23:31:02 +0530

    Print all the permutations of characters in a String. Added comments

--- Array/StringPermutations.java
+++ Array/StringPermutations.java
@@ -1,7 +1,48 @@
 package Array;
 
 import static Array.ArrayUtility.swap;
-
+/***
+ * PROBLEM :
+ *
+ * We are given a input string and we are asked to print all the permutations
+ * of the characters in it.
+ *
+ * SOLUTION :
+ *
+ * We fix the characters till an index and permute the remaining characters.
+ * At each recursive call we fix characters till one more index than the
+ * previous level.
+ *
+ * Let us consider an example : ABC
+ *
+ *        - We fix A at index 0 and permute  [ B,C ]
+ *              - We fix B at index 1 and permute [ C ]
+ *                         - We fix C at index 2
+ *                         - Print ABC
+ *
+ *              - We fix C at index 1 and permute [ B ]
+ *                         - We fix B at index 2
+ *                         - Print ACB
+ *
+ *        - We fix B at index 0 and permute  [ A,C ]
+ *              - We fix A at index 1 and permute [ C ]
+ *                      - We fix C at index 2
+ *                      - Print BAC
+ *
+ *              - We fix C at index 1 and permute [ A ]
+ *                      - We fix A at index 2
+ *                      - Print BCA
+ *
+ *        - We fix C at index 0 and permute  [ A,B ]
+ *              - We fix A at index 1 and permute [ B ]
+ *                      - We fix B at index 2
+ *                      - Print CAB
+ *
+ *              - We fix B at index 1 and permute [ A ]
+ *                      - We fix A at index 2
+ *                      - Print CBA
+ *
+ * */
 public class StringPermutations {
 
     public static void main(String args[]){
