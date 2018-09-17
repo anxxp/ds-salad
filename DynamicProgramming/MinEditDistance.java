f50705d3cdf3aa1d28c62fb0c3ab174d8b82d773
anoop29 <anoopkumar.inbox@gmail.com>
2018-09-18 00:43:24 +0530

    Minimum edit distance

--- DynamicProgramming/MinEditDistance.java
+++ DynamicProgramming/MinEditDistance.java
@@ -1,5 +1,15 @@
 package DynamicProgramming;
-
+/**
+ * PROBLEM:
+ *
+ * Given two strings str1 and str2 and below operations that can performed on str1.
+ * Find minimum number of edits (operations) required to convert ‘str1’ into ‘str2’.
+ *  1. Insert
+ *  2. Remove
+ *  3.Replace
+ *
+ *
+ * */
 public class MinEditDistance {
 
     public static void main(String args[]){
