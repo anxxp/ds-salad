77eb5dab0bac4e3d480059f4f01a4ba3a144f742
anoop29 <anoopkumar.inbox@gmail.com>
2019-03-30 13:25:23 +0530

    Trapping rain water

---
+++ Leetcode/src/Misc/TrappingRainWater.java
@@ -0,0 +1,35 @@
+package Misc;
+
+/**
+ *
+ Given n non-negative integers representing an
+ elevation map where the width of each bar is 1,
+ compute how much water it is able to trap after
+ raining.
+
+    ^
+ 13	|
+ 12	|	 11
+ 11	|	HHH
+ 10	|	HHH          9
+ 9	|	HHH----8----HHH
+ 8	|	HHH---HHH---HHH       7
+ 7	|	HHH---HHH---HHH------HHH
+ 6	|	HHH---HHH---HHH-5----HHH
+ 5	|	HHH-6-HHH---HHHHHH---HHH
+ 4	|	HHHHHHHHH---HHHHHH-3-HHH
+ 3	|	HHHHHHHHH 2 HHHHHHHHHHHH 2
+ 2	|	HHHHHHHHHHHHHHHHHHHHHHHHHHH 1
+ 1	|_0_HHHHHHHHHHHHHHHHHHHHHHHHHHHHH_0_
+     ---------------------------------------->
+ The above elevation map is represented by
+ array [0,11,6,8,2,9,5,3,7,2,1,0].
+
+ Water is represented by hyphen.
+
+*/
+
+public class TrappingRainWater {
+
+
+}
