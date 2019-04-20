3b01c53f0719a3f82fee6d28dd9d01f4171d7bcc
anoop29 <anoopkumar.inbox@gmail.com>
2019-04-21 00:40:06 +0530

    Better and clean code for Smallest Element Greater than Equal to a given num. If there is no such number then the code return the Integer.MIN_VALUE

--- Array/SmallestElementGreaterEqualToNum.java
+++ Array/SmallestElementGreaterEqualToNum.java
@@ -5,10 +5,11 @@ public class SmallestElementGreaterEqualToNum {
     public static void main(String args[]){
 
         int[] input = {10,12,15,18,25,33};
-        int num = 30;
+        int num = 5;
 
         System.out.println(smallestElementGreaterThanEqual(input,num));
     }
+
 
     private static int smallestElementGreaterThanEqual(int[] input, int num) {
 
@@ -20,37 +21,24 @@ public class SmallestElementGreaterEqualToNum {
         int end = input.length-1;
         int mid;
 
-        while(start<=end){
+        while(start+1 < end){
+            mid = start + (end - start)/2;
 
-            mid = (start+end)/2;
-
-            if(start == end){
-                if(input[mid]>=num){
-                    return mid;
-                }else{
-                    return -1;
-                }
-            }
-
-
-            if(end == start+1){
-                if(input[start]>=num){
-                    return start;
-                }else if(input[end]>=num){
-                    return end;
-                }else{
-                    return -1;
-                }
-            }
-
-            if(input[mid]>=num && input[mid-1]<num ){
-                return mid;
+            if(input[mid] >= num && input[mid-1] < num){
+                return input[mid];
             }else if(input[mid] > num){
                 end = mid-1;
             }else{
                 start = mid+1;
             }
         }
-        return -1;
+
+        if(input[start]>= num){
+            return input[start];
+        }else if(input[end] >=num){
+            return input[end];
+        }else{
+            return Integer.MIN_VALUE;
+        }
     }
 }
