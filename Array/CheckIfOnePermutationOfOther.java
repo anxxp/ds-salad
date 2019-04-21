8106d2c8f237021e39be4790ab2e6f53567fef16
anoop29 <anoopkumar.inbox@gmail.com>
2019-04-21 10:38:27 +0530

    Added comments to optimize the memory requirement. :D

--- Array/CheckIfOnePermutationOfOther.java
+++ Array/CheckIfOnePermutationOfOther.java
@@ -15,6 +15,19 @@ public class CheckIfOnePermutationOfOther {
             return false;
         }
 
+        /**
+         * There are two optimizations that we can do while creating the array
+         * 1. If the total number of characters in the input string is
+         *      less than 127 then, we can use
+         *      byte[] count = new byte[128]; byte 1 byte  -128 to 127
+         *      less than 32,767 then , we can use
+         *      short[] count = new short[128];
+         *
+         * 2. If we know the character set
+         *      if only alphabets and all in lower case then
+         *      array of size 26 is only needed and we can calculate
+         *      the index in the array by subtracting 97; as a->97 in ASCII
+         * */
         int[] count = new int[128];
 
         for (int i:inputStr1.toCharArray() ) {
